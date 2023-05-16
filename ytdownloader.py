2fcc14f59810b6d0d59aeb600d74740ca7fc9744
chidalu <chidalu.chukwuneke@downergroup.com>
2023-05-17 08:53:19 +1000

    added os dir path

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,10 +1,18 @@
 from pytube import YouTube
+import os
 from sys import argv
 
 link = argv[1]
+
+cwd = os.getcwd()
+dir_path = os.path.join(cwd, "youtube_videos")
+
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
 
 yt = YouTube(link)
 print("Title: ", yt.title)
 print("View, ", yt.views)
 yd = yt.streams.get_highest_resolution()
-yd.download(r"C:\Users\Chidalu.Chukwuneke\Documents\Projects\yt_downloader\videos")
+# yd.download(r"C:\Users\Chidalu.Chukwuneke\Documents\Projects\yt_downloader\videos")
+yd.download(dir_path)
